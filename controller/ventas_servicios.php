5f0214b625cff29e5da192ad071d3915c0da9019
NeoRazorX <neorazorx@gmail.com>
2016-11-23 18:12:31 +0100

    Pequeña corrección al filtrar por fecha de inicio.

--- controller/ventas_servicios.php
+++ controller/ventas_servicios.php
@@ -235,7 +235,7 @@ class ventas_servicios extends fs_controller
             {
                $this->fechainicio = $_REQUEST['fechainicio'];
             }
-            if( isset($_REQUEST['fechainicio']) )
+            if( isset($_REQUEST['fechafin']) )
             {
                $this->fechafin = $_REQUEST['fechafin'];
             }
